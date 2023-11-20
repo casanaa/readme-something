4bc52f3e94157abc806d98f924e122155392cc20
casanaa <antoniocasana@yahoo.com>
2023-11-20 13:37:06 -0600

    added code

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// const generateMarkdown = require('./utils/generateMarkdown');
+const renderLicense = require('./utils/generateMarkdown');
+// const renderLicenseLink = require('./utils/generateMarkdown');
+
+
+
 const questions = [
     {
         type: "input",
@@ -72,15 +81,63 @@ const questions = [
     },
 ];
 
-inquirer.prompt(questions).then(answers);  {
+inquirer
+.prompt(questions)
+.then((answers) =>  {
     const markdown = getMarkdown(answers);
-    console.log(Success)
-
-fs.writeFile(fileName, generateMarkdown(data), function (err) {
+    
+fs.writeFile('README.md', markdown, (err) => {
     if (err) {
-        return console.log(err);
-    } 
+        console.error(err);
+    } else {
+        console.log(Success);
+    }
 });
+});
+
+const generateMarkdown = (answers) => {
+    const { title, description, toc1, toc2, toc3, toc4, toc5, installation, usage, license, contribution, tests, email, questions } = answers;
+
+    return `
+#  Title
+    ${title} 
+
+## ${renderLicense(license)}
+
+## Description
+    ${description}
+
+## Table of Contents
+   1. ${toc1} 
+   2. ${toc2}
+   3. ${toc3}
+   4. ${toc4}
+   5. ${toc5}
+
+
+## Installation
+    ${installation}
+
+## Usage
+    ${usage}
+
+## Contributtion
+    ${contribution}
+
+## Tests
+    ${tests}
+
+## Questions
+    ${questions}
+    * https://github.com/casanaa 
+    ${email} 
+    * Please email me with any questions you may have. 
+`;
+}
+
+// TODO: Create a function to initialize app
+function init() { 
+    askQuestions();
 };
 
 // Function call to initialize app
